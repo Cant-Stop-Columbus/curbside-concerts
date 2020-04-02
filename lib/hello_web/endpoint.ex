035ec8bcf87a509f1709eb4d3056ac0ad8530961
Marc Peabody <marcpeabody@yahoo.com>
2020-04-02 13:16:09 -0400

    websockets need longer timeout

--- lib/hello_web/endpoint.ex
+++ lib/hello_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule HelloWeb.Endpoint do
   ]
 
   socket "/socket", HelloWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
