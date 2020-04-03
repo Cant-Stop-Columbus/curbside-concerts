230d26105ccf8ac2412b4eb2dccc7f34dab55115
bannmoore <moore.brittanyann@gmail.com>
2020-04-03 15:34:49 -0400

    Add alt text to hero image on request form.
    Side Effects:
    - Restrict width of form on larger screens.

--- lib/hello_web/views/request_view.ex
+++ lib/hello_web/views/request_view.ex
@@ -68,6 +68,15 @@ defmodule HelloWeb.RequestView do
     """
   end
 
+  def hero_image() do
+    alt_text =
+      "A man sitting in the bed of a pickup truck plays guitar for an elderly woman standing in her driveway. A speech bubble over the man says \"That song was for you. People care about you. This is from your daughter: 'Mom, we know it's hard to be alone but we want you to be safe. We hope this song brightens your day. We love you.'\""
+
+    ~E"""
+    <img src="./images/for_you.png" alt="<%= alt_text %>" />
+    """
+  end
+
   defp class(form, field) do
     if form.errors[field], do: "not-valid", else: ""
   end
