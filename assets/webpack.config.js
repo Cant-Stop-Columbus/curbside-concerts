6b3e5d8b49aaf65431b9e187099f02a9064da491
bannmoore <moore.brittanyann@gmail.com>
2020-04-03 10:22:01 -0400

    Add normalize.css

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -37,5 +37,5 @@ module.exports = (env, options) => ({
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
+	]
 });
