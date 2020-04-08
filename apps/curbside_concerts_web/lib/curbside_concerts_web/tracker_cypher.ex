0fe5ed583933ce5aa5d9a825276e04e94078d699
Marc Peabody <marcpeabody@yahoo.com>
2020-04-08 17:02:25 -0400

    Setting up environment salt with default fallback

--- apps/curbside_concerts_web/lib/curbside_concerts_web/tracker_cypher.ex
+++ apps/curbside_concerts_web/lib/curbside_concerts_web/tracker_cypher.ex
@@ -7,12 +7,15 @@ defmodule CurbsideConcertsWeb.TrackerCypher do
   The cypher is function instead of a module attribute to avoid problems
   with looking for environment variables.
   """
+  @default_salt "12mp34"
   @min_length 12
 
   defp cypher do
+    system_salt = System.get_env("TRACKER_CYPHER_SALT")
+
     Hashids.new(
       # using a custom salt helps producing unique cipher text
-      salt: "123456",
+      salt: system_salt || @default_salt,
       # minimum length of the cipher text (1 by default)
       min_len: @min_length
     )
@@ -23,13 +26,12 @@ defmodule CurbsideConcertsWeb.TrackerCypher do
   end
 
   def decode(code) when is_binary(code) do
-    # if String.length(code) == @min_length do
-    cypher()
-    |> Hashids.decode!(code)
-    |> List.first()
-
-    # else
-    #   0
-    # end
+    if String.length(code) == @min_length do
+      cypher()
+      |> Hashids.decode!(code)
+      |> List.first()
+    else
+      0
+    end
   end
 end
