af4f9cbf04a3ba6b411d6cb64784f443f30fe6c2
bannmoore <moore.brittanyann@gmail.com>
2020-04-14 23:33:38 -0400

    Load nprogress from js, rather than in our app css.

--- apps/curbside_concerts_web/assets/js/app.js
+++ apps/curbside_concerts_web/assets/js/app.js
@@ -1,8 +1,9 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "normalize.css"
-import "../css/app.scss"
+import "normalize.css";
+import "../node_modules/nprogress/nprogress.css";
+import "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -10,33 +11,35 @@ import "../css/app.scss"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-import Hooks from "./booker"
+import Hooks from "./booker";
 
-import { Socket } from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import { Socket } from "phoenix";
+import LiveSocket from "phoenix_live_view";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+let csrfToken = document
+	.querySelector("meta[name='csrf-token']")
+	.getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken },
-  hooks: Hooks
-})
+	params: { _csrf_token: csrfToken },
+	hooks: Hooks,
+});
 
-import NProgress from "nprogress"
+import NProgress from "nprogress";
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
+window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
