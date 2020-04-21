fc9e8419a00a2b555129ec9c11a369c3c931b8b8
neilnagarsheth <neilnagarsheth@gmail.com>
2020-04-21 12:43:14 -0400

    adding more assertions to the email request test

--- apps/curbside_concerts_web/test/email/email_request_test.exs
+++ apps/curbside_concerts_web/test/email/email_request_test.exs
@@ -6,7 +6,6 @@ defmodule CurbsideConcertsWeb.EmailRequestTest do
 
   alias CurbsideConcerts.Requests.Request
   alias CurbsideConcerts.Musicians.Session
-  alias CurbsideConcerts.Musicians.Musician
   alias CurbsideConcerts.Repo
   alias CurbsideConcertsWeb.EmailRequest
 
@@ -18,10 +17,14 @@ defmodule CurbsideConcertsWeb.EmailRequestTest do
     test "sends the email" do
       %Session{id: session_id} = insert!(:session)
       insert!(:musician, %{ session_id: session_id})
-      %Request{id: request_id} = insert!(:request, %{ session_id: session_id})
+      %Request{id: request_id, requester_email: email_address} = insert!(:request, %{ session_id: session_id})
         |> Repo.preload([{:session, :musician}])
       expected_email = EmailRequest.send_session_booked(request_id)
+
       assert_delivered_email expected_email
+      assert expected_email.to == [{nil, email_address}]
+      assert expected_email.from == {"Curbside Concerts", "testuser@sendaconcert.com"}
+      assert expected_email.html_body != nil
     end
   end
 end
